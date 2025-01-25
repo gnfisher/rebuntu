0b9fbd1c16d9bcf0f78349f3d2df64a225cf5e04
Greg Fisher <greg@gnfisher.com>
2025-01-25 13:31:23 -0500

    Remove gum

--- install/desktop.sh
+++ install/desktop.sh
@@ -8,6 +8,3 @@ for installer in ~/.local/share/rebuntu/install/desktop/*.sh; do source $install
 # Revert to normal idle and lock settings
 gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.session idle-delay 300
-
-# Logout to pickup changes
-gum confirm "Ready to reboot for all settings to take effect?" && sudo reboot
