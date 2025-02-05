19b0dce6f554f5e2cbf24318fdb5fbd2264461c4
Greg Fisher <greg@gnfisher.com>
2025-02-05 13:10:42 -0500

    Delete install/desktop/set-wallpaper.sh

--- install/desktop/set-wallpaper.sh
+++
@@ -1,3 +0,0 @@
-# Set wallpaper
-gsettings set org.gnome.desktop.background picture-uri "file://$HOME/.local/share/rebuntu/esxtra/nostalgia-wallpaper.jpg"
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$HOME/.local/share/rebuntu/extra/nostalgia-wallpaper.jpg"
