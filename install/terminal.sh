b7d7ec76203c0222036f141b84f812be41ef7a97
Greg Fisher <greg@gnfisher.com>
2025-02-05 18:29:40 -0500

    Changes

--- install/terminal.sh
+++ install/terminal.sh
@@ -5,3 +5,9 @@ sudo apt install -y curl git unzip
 
 # Run terminal installers
 for installer in ~/.local/share/rebuntu/install/terminal/*.sh; do source $installer; done
+
+# Symlink new bashrc
+if [ -f ~/.bashrc ]; then
+  mv ~/.bashrc ~/.bashrc.bak
+fi
+ln -s ~/.local/share/rebuntu/configs/bashrc ~/.bashrc
